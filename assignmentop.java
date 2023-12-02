390a68b2cad54465af0930297043c583634da33c
jayalakshmi31 <112812081+jayalakshmi31@users.noreply.github.com>
2023-12-02 12:50:12 +0530

    Add files via upload

---
+++ assignmentop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Pc
+ */
+public class assignmentop {
+ public static void main(String[] agrs){
+     int a=9;
+     a+=3;
+     
+ }   
+}
